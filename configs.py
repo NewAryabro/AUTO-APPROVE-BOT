4b9cd0c0c923bcb913ee18018fa6e36d0f7c3cba
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2024-12-29 10:55:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "dbd251e9ad4883b0443cc82b618ac6fa")
     BOT_TOKEN = getenv("BOT_TOKEN", "6841653877:AAEfhfpd6wNs7sfWOsson-8yBXOvRq328Rk")
     FSUB = getenv("FSUB", "Telugu_Movies_999")
-    CHID = int(getenv("CHID", "-1002292066966"))
+    CHID = int(getenv("CHID", "-1001798300759"))
     SUDO = list(map(int, getenv("SUDO", "6081617163").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://bestanimeandcartoonsclips:6ChCTALD96W7qkps@cluster0.dt9r1rc.mongodb.net/?retryWrites=true&w=majority")
     
