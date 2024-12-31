6a96095a3cd7acf44b4acca82d0fb23945127545
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2024-12-31 07:13:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "7515868"))
     API_HASH = getenv("API_HASH", "dbd251e9ad4883b0443cc82b618ac6fa")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7619312091:AAHgu5KM8zQTuH7F8CN-Md7D0VXBqlF9jno")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7619312091:AAF47pVl0SYnVGW2aqYT8uVUEiQJ2uJan7g")
     FSUB = getenv("FSUB", "Telugu_Movies_999")
     CHID = int(getenv("CHID", "-1001798300759"))
     SUDO = list(map(int, getenv("SUDO", "6081617163").split()))
